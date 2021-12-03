437ab9b42852d415f8810a9deac99734436b2b73
Gaoxusheng <2425354040@qq.com>
2021-12-04 07:34:09 +0800

    add block

--- fastJson/app.js
+++ fastJson/app.js
@@ -1,25 +1,43 @@
 const fs = require('fs');
 const mod = "more_blocks"
-const data = ["leaf","test_block","b","bl","g","gg","o","r","w","x","y"]
-for(let i =0;i<data.length;i++){
-    fs.writeFileSync(`../src/generated/resources/assets/more_blocks/blockstates/${data[i]}.json`,JSON.stringify(
-    {
-        "variants": {
-            "normal": { "model": `${mod}:${data[i]}` }
-        }
-    }
-    ))
-    fs.writeFileSync(`../src/generated/resources/assets/more_blocks/models/block/${data[i]}.json`,JSON.stringify(
-        {
-            "parent": "block/cube_all",
-            "textures": {
-              "all": `${mod}:blocks/${data[i]}`
-            }
-          }
-    ))
-    fs.writeFileSync(`../src/generated/resources/assets/more_blocks/models/item/${data[i]}.json`,JSON.stringify(
-    {
-      "parent": `${mod}:block/${data[i]}`
-    }
-    ))
+function DirData() {
+  return new Promise((res, rej) => {
+    fs.readdir(`../src/generated/resources/assets/${mod}/textures/blocks`, (e, r) => {
+      if (e) rej(e);
+      res(r)
+    })
+  })
 }
+
+DirData().then(r => {
+  let data = r.map(x => x.substring(0, x.length - 4))
+  console.log(data);
+  console.log(data.length);
+  for (let i = 0; i < data.length; i++) {
+    fs.writeFileSync(`../src/generated/resources/assets/${mod}/blockstates/${data[i]}.json`, JSON.stringify(
+      {
+        "variants": {
+          "normal": [
+            { "model": `${mod}:${data[i]}` },
+            { "model": `${mod}:${data[i]}`, "y": 90 },
+            { "model": `${mod}:${data[i]}`, "y": 180 },
+            { "model": `${mod}:${data[i]}`, "y": 270 }
+          ]
+        }
+      }
+    ))
+    fs.writeFileSync(`../src/generated/resources/assets/${mod}/models/block/${data[i]}.json`, JSON.stringify(
+      {
+        "parent": "block/cube_all",
+        "textures": {
+          "all": `${mod}:blocks/${data[i]}`
+        }
+      }
+    ))
+    fs.writeFileSync(`../src/generated/resources/assets/${mod}/models/item/${data[i]}.json`, JSON.stringify(
+      {
+        "parent": `${mod}:block/${data[i]}`
+      }
+    ))
+  }
+})
--- src/main/java/yunmouren/top/more_blocks/Register/BlockList/BlockItems.java
+++ src/main/java/yunmouren/top/more_blocks/Register/BlockList/BlockItems.java
@@ -12,14 +12,19 @@ public class BlockItems {
     public static final List<Block> blocks = new ArrayList<>();
     //    注册物品
     public static final Block test_block = new FastBlock("test_block");
-    public static final Block leaf = new FastBlock("leaf");
-    public static final Block b = new FastBlock("b");
-    public static final Block bl = new FastBlock("bl");
-    public static final Block g = new FastBlock("g");
-    public static final Block gg = new FastBlock("gg");
-    public static final Block o = new FastBlock("o");
-    public static final Block r = new FastBlock("r");
-    public static final Block w = new FastBlock("w");
-    public static final Block x = new FastBlock("x");
-    public static final Block y = new FastBlock("y");
+    public static final Block red = new FastBlock("red");
+    public static final Block block_yellow = new FastBlock("block_yellow");
+    public static final Block orange = new FastBlock("orange");
+    public static final Block cyan = new FastBlock("cyan");
+    public static final Block brick = new FastBlock("brick");
+    public static final Block blue_glass = new FastBlock("blue_glass");
+    public static final Block golden_leaves = new FastBlock("golden_leaves");
+    public static final Block white_wood = new FastBlock("white_wood");
+    public static final Block white_brick = new FastBlock("white_brick");
+    public static final Block grassland = new FastBlock("grassland");
+    public static final Block autumn_grassland = new FastBlock("autumn_grassland");
+    public static final Block purple_grass = new FastBlock("purple_grass");
+    public static final Block blue_grass = new FastBlock("blue_grass");
+    public static final Block arrow = new FastBlock("arrow");
+
 }
