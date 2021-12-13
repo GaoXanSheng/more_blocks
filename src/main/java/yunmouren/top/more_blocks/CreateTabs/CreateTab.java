3f423c574f3ff9f371a308c24c70c0f4a4ecc063
Gaoxusheng <moxiaojulove@foxmail.com>
2021-12-13 13:19:11 +0800

    Rescue blocks recovered from data Inadvertently click the consequences of deletion

--- src/main/java/yunmouren/top/more_blocks/CreateTabs/CreateTab.java
+++ src/main/java/yunmouren/top/more_blocks/CreateTabs/CreateTab.java
@@ -1,13 +1,15 @@
 package yunmouren.top.more_blocks.CreateTabs;
 
+import com.sun.istack.internal.NotNull;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import yunmouren.top.more_blocks.Register.BlockList.BlockItems;
 
 public class CreateTab extends CreativeTabs {
     @Override
+    @NotNull
     public ItemStack createIcon() {
-        return new ItemStack(Items.CLOCK);
+        return new ItemStack(BlockItems.command_block);
     }
     public CreateTab (){
         super("more_blocks");
